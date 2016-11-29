690364673fb50eb911e3d1fe24aa34adb5000044
charis <charis@phantom-nx>
2016-11-29 17:40:43 +0200

    New sub-letter classes TODO:

---
+++ CryptoCross/src/cryptocross/RedLetter.java
@@ -0,0 +1,26 @@
+/*
+ * @author Charis Ioannikios Kaonis 321/2010069
+ * @author Georgios Moustakas 321/2011102
+ * @project CryptoCross (Κρυπτόλεξο) 2016
+ */
+package cryptocross;
+
+//Class of a single letter
+public class RedLetter extends Letter {
+
+    public RedLetter(Letter.Alphabet enum_letter) {
+        super(enum_letter);
+        //this.ch_letter = ch_letter;
+        //this.enum_letter = enum_letter;
+    }
+
+    public RedLetter(Character ch_letter) {
+        super(ch_letter);
+    }
+
+    @Override
+    public void setPoints(Integer int_points) {
+        this.int_points = 2 * int_points;
+    }
+
+}
